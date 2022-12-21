67a5b520af446603ea0237aca923917da5e757df
hssj-sw <12067556+hs-sw@user.noreply.gitee.com>
2022-12-21 14:17:07 +0800

    纹理

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -14,20 +14,30 @@
 ANativeWindow *nativeWindow = NULL;
 WlEglThread *wlEglThread = NULL;
 
-const char *vertex = "attribute vec4 a_position;\n"
-                     "\n"
-                     "void main(){\n"
-                     "    gl_Position = a_position;\n"
+const char *vertex = "attribute vec4 v_Position;\n"
+                     "attribute vec2 f_Position;\n"
+                     "varying vec2 ft_Position;\n"
+                     "void main() {\n"
+                     "    ft_Position = f_Position;\n"
+                     "    gl_Position = v_Position;\n"
                      "}";
 
-const char *fragment = "precision mediump float;\n"
-                       "\n"
-                       "void main(){\n"
-                       "    gl_FragColor = vec4(1f,0f,0f,1f);\n"
-                       "}";
+const char *fragment =  "precision mediump float;\n"
+                        "varying vec2 ft_Position;\n"
+                        "uniform sampler2D sTexture;\n"
+                        "void main() {\n"
+                        "    gl_FragColor=texture2D(sTexture, ft_Position);\n"
+                        "}";
 
 int program;
 GLint vPosition;
+GLint fPosition;
+GLint sampler;
+GLuint textureId;
+
+int w;
+int h;
+void *pixels = NULL;
 
 float vertexs [] ={
         1,-1,
@@ -36,13 +46,35 @@ float vertexs [] ={
         -1,1
 };
 
+float fragments[]={
+        1,1,
+        1,0,
+        0,1,
+        0,0
+};
+
 void callback_SurfaceCrete(void *ctx){
     LOGD("callback_SurfaceCreate")
     WlEglThread *wlEglThread = static_cast<WlEglThread*>(ctx);
 
     program = createProgrm(vertex,fragment);
     LOGD("OPENGL program is %d",program);
-    vPosition = glGetAttribLocation(program,"a_position");
+    vPosition = glGetAttribLocation(program,"v_Position");
+    fPosition = glGetAttribLocation(program,"f_Position");
+    sampler = glGetUniformLocation(program,"sTexture");
+
+    glGenTextures(1,&textureId);
+
+    glBindTexture(GL_TEXTURE_2D,textureId);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S,GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T,GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+
+    if (pixels != NULL){
+        glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA,w,h,0,GL_RGBA,GL_UNSIGNED_BYTE,pixels);
+    }
+    glBindTexture(GL_TEXTURE_2D,0);
 }
 
 void callback_SurfacChange(int width ,int height,void *ctx){
@@ -58,9 +90,20 @@ void callback_SurfaceDraw(void  *ctx){
     glClearColor(0.0f,0.0f,0.0f,1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
     glUseProgram(program);
+
+    glActiveTexture(GL_TEXTURE5);
+    glUniform1i(sampler,5);
+    glBindTexture(GL_TEXTURE_2D,textureId);
+
     glEnableVertexAttribArray(vPosition);
     glVertexAttribPointer(vPosition,2,GL_FLOAT, false,8,vertexs);
-    glDrawArrays(GL_TRIANGLE_STRIP,0,3);
+
+    glEnableVertexAttribArray(fPosition);
+    glVertexAttribPointer(fPosition,2,GL_FLOAT,false,8,fragment);
+
+    glDrawArrays(GL_TRIANGLE_STRIP,0,4);
+
+    glBindTexture(GL_TEXTURE_2D,0);
 }
 
 
@@ -85,4 +128,17 @@ Java_com_imooic_opengl_NativeOpengl_surfaceChange(JNIEnv *env, jobject thiz, jin
         usleep(1000000);
         wlEglThread ->notifyRender();
     }
+}
+extern "C"
+JNIEXPORT void JNICALL
+Java_com_imooic_opengl_NativeOpengl_imgData(JNIEnv *env, jobject thiz, jint width, jint height, jint length,
+                                            jbyteArray data_) {
+    jbyte  *date = env ->GetByteArrayElements(data_,NULL);
+
+    w = width;
+    h = height;
+    pixels = malloc(length);
+    memcpy(pixels,date,length);
+
+    env ->ReleaseByteArrayElements(data_,date,0);
 }
--- app/src/main/java/com/imooic/openglnativedemo/MainActivity.java
+++ app/src/main/java/com/imooic/openglnativedemo/MainActivity.java
@@ -1,5 +1,7 @@
 package com.imooic.openglnativedemo;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
@@ -7,6 +9,8 @@ import android.widget.TextView;
 import com.imooic.opengl.NativeOpengl;
 import com.imooic.opengl.WlSurfaceView;
 import com.imooic.openglnativedemo.databinding.ActivityMainBinding;
+
+import java.nio.ByteBuffer;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,6 +26,13 @@ private ActivityMainBinding binding;
      binding = ActivityMainBinding.inflate(getLayoutInflater());
      setContentView(binding.getRoot());
         binding.wlsurfaceview.setNativeOpengl(nativeOpengl = new NativeOpengl());
+
+        final Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.mingren);
+        ByteBuffer fcbuffer = ByteBuffer.allocate(bitmap.getHeight()*bitmap.getWidth()*4);
+        bitmap.copyPixelsToBuffer(fcbuffer);
+        fcbuffer.flip();
+        byte[] pixels = fcbuffer.array();
+        nativeOpengl.imgData(bitmap.getWidth(),bitmap.getHeight(),pixels.length,pixels);
     }
 
 
